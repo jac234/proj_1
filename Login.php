f290e763ccde02aa26711404096dd2de59eb2181
Jefferson A Calle <jac234@afsconnect1.njit.edu>
2020-10-29 19:27:54 -0400

    added validations to Login

--- Login.php
+++ Login.php
@@ -4,9 +4,21 @@
     $email = filter_input(INPUT_POST, 'email');
     $pass  = filter_input(INPUT_POST, 'pass');
 
-    //using an array to hold values
-    $login = array("$email", "$pass");
+    //Cant be empty & contains @ character
+    if((strlen($email) > 0) and (strpos($email, "@") != FALSE)){
+        echo "Email Address: $email <br><br>";
+    }elseif (strlen($email) == 0){
+        echo "Email Address: $email<br> - should not be left empty.<br><br>";
+    }elseif (strpos($email, "@") != TRUE){
+        echo "Email Address: $email<br> - should contain '@' symbol.<br><br>";
+    }
 
-    echo "$login[0] <br><br>"; //Cant be empty & contains @ character
-    echo "$login[1] <br><br>"; //Cant be empty & at least 8 character
+    //Cant be empty & at least 8 character
+    if((strlen($pass) > 0) and (strlen($pass) >= 8)){
+        echo "Password: $pass <br><br>";
+    }elseif ((strlen($pass) == 0)){
+        echo "Password: $pass<br> - should not be left empty.<br><br>";;
+    }elseif ((strlen($pass) < 8)){
+        echo "Password: $pass<br> - should be 8 characters or longer.<br><br>";;
+    }
 ?>
